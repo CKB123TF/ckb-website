abfc620b6e9ffcd4fc90a354f4d5c98f31554c7f
Christian Kildal-Brandt <ckildalbrandt@gmail.com>
2024-01-12 23:09:04 -0500

    changed text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -273,13 +273,10 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Software designer, founder, and amateur astronaut.
+            NYC Software Engineer, Writer, and Founder
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Christian, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            "All great literature is one of two stories; a man goes on a journey or a stranger comes to town." Leo Tolstoy
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
