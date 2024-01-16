758c9662448d64a74a7ba1ebefd4f66b00e9cd25
Christian Kildal-Brandt <ckildalbrandt@gmail.com>
2024-01-16 18:07:49 -0500

    text change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -271,7 +271,7 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            NYC Software Engineer, Writer, and Founder
+            NYC Based Software Engineer, Writer, and Founder
           </h1>
           <p className="mt-6 text-base text-zinc-800 dark:text-zinc-400">
             ’All great literature is one of two stories; a man goes on a journey or a stranger comes to town.’ - Leo Tolstoy
